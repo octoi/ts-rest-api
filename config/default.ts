f5040526c4fc9d65843df3cfc5c280cc1139b006
fadhilsaheer <fadhilsaheer@gmail.com>
2021-10-05 13:10:11 +0530

    🏆

--- config/default.ts
+++ config/default.ts
@@ -4,4 +4,5 @@ export default {
   port: 5000,
   host: 'localhost',
   dbUri: 'mongodb://localhost:27017/rest-api', // mongo db connection url
+  privateKey: 'never gonna give you up', // change this
 };
